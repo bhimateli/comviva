fbdd1de604218db415da40947aece414c056f8fe
Bhimashankar Teli <bteli.selenium@gmail.com>
2024-12-08 13:56:57 +0530

    Updated DB util

--- src/main/java/util/DBUtility.java
+++ src/main/java/util/DBUtility.java
@@ -3,9 +3,8 @@ package util;
 @author Bhimashankar Teli
  */
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import javax.xml.transform.Result;
+import java.sql.*;
 
 public class DBUtility {
 
@@ -17,18 +16,38 @@ public class DBUtility {
 
 
     // Method to establish a database connection
-    public void connect() throws SQLException, SQLException, ClassNotFoundException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        if (connection == null || connection.isClosed()) {
-            connection = DriverManager.getConnection(url, username, password);
-        }
-    }
+
 
     // Method to disconnect from the database
     public void disconnect() throws SQLException {
         if (connection != null && !connection.isClosed()) {
             connection.close();
         }
+    }
+
+    /*
+    to get connection - we can use it any class
+     */
+    public static Connection getConenction(){
+
+         String userName = "bteli";
+         String password = "admin";
+        String url = "jdbc:mysql://localhost:3306/mydatabase";
+        Connection con= null;
+
+        try{
+            Class.forName("com.mysql.cj.jdbc.driver");
+             con = DriverManager.getConnection(url,userName,password);
+          //  Statement stmt = con.createStatement();
+         //   ResultSet rs = stmt.executeQuery("select * from table");
+
+
+
+        }
+        catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Error executing query: " + e.getMessage());
+        }
+    return con;
     }
 
 }
